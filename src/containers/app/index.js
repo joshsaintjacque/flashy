6040be38c941a4f3ca4c3fd30bd573ce6775b2f1
Josh Saint Jacque <joshsaintjacque@gmail.com>
2018-03-12 21:50:09 -0700

    Update routes with subfolder

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -6,13 +6,13 @@ import About from '../about';
 const App = () => (
   <div>
     <header>
-      <Link to="/">Home</Link>
-      <Link to="/about-us">About</Link>
+      <Link to="/flashy">Home</Link>
+      <Link to="/flashy/about-us">About</Link>
     </header>
 
     <main>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about-us" component={About} />
+      <Route exact path="/flashy" component={Home} />
+      <Route exact path="/flashy/about-us" component={About} />
     </main>
   </div>
 );
